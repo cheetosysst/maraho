2ddda69a7e8d8b5fe6b5a87c09da3de663a94cab
Thect <thect@protonmail.com>
2022-06-29 11:24:23 +0800

    Navbar Links Links are now generated with datas in config.js Responsive design

--- components/main.layout.js
+++ components/main.layout.js
@@ -1,9 +1,27 @@
+import { useState, useEffect } from "react";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import config from "../config";
 import Navbar from "./navbar.component.js";
 
 export default function Layout({ children }) {
+
+	const [links, setLinks] = useState([]);
+
+	useEffect(() => {
+		let tempLinks = [];
+		for (let i = 0; i < config.navbar.list.length; i++) {
+			tempLinks.push(
+				<li className="uppercase">
+					<Link href={"/" + config.navbar.list[i]}>
+						{config.navbar.list[i]}
+					</Link>
+				</li>
+			);
+		}
+		setLinks(tempLinks);
+	}, []);
 
 	return (
 		<>
@@ -13,8 +31,27 @@ export default function Layout({ children }) {
 					content={config.meta.description}
 				/>
 			</Head>
-			<Navbar/>
-			{children}
+			<div className="drawer">
+				<input
+					id="my-drawer"
+					type="checkbox"
+					className="drawer-toggle"
+				/>
+				<div className="drawer-content">
+					<Navbar />
+					{children}
+				</div>
+				<div className="drawer-side">
+					<label
+						htmlFor="my-drawer"
+						className="drawer-overlay"
+					></label>
+					<ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+						<h1 className="text-2xl ml-3 my-3">Category</h1>
+						{links}
+					</ul>
+				</div>
+			</div>
 		</>
 	);
 }
--- components/navbar.component.js
+++ components/navbar.component.js
@@ -23,12 +23,33 @@ export default function Navbar() {
 		<>
 			<div className="px-4">
 				<div className="navbar bg-base-300 mt-3 rounded-xl shadow">
+					<div className="flex-none md:hidden">
+						<label
+							htmlFor="my-drawer"
+							className="btn btn-square btn-ghost rounded-xl shadow drawer-button"
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								className="inline-block w-5 h-5 stroke-current"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth="2"
+									d="M4 6h16M4 12h16M4 18h16"
+								></path>
+							</svg>
+						</label>
+					</div>
+
 					<div className="flex-1">
 						<div className="btn btn-ghost text-xl uppercase tracking-wide">
 							<Link href={"/"}>{config.name}</Link>
 						</div>
 					</div>
-					<div className="flex-none">
+					<div className="flex-none md:visible invisible">
 						<ul className="menu menu-horizontal p-0">{links}</ul>
 					</div>
 				</div>

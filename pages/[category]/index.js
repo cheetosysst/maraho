2b444dee356396d28634c0ddbb7184aefcaed940
Thect <thect@protonmail.com>
2022-07-04 13:15:12 +0800

    Basic category page

--- pages/[category]/index.js
+++ pages/[category]/index.js
@@ -1,19 +1,71 @@
 import Head from "next/head";
+import Image from "next/image";
 import config from "../../config";
 import Layout from "../../components/main.layout";
 import fs from "fs";
 import path from "path";
 import { useState, useEffect } from "react";
+import Link from "next/link";
 
 export default function Category({ id, articles, category }) {
+	const [articleCards, setArticleCards] = useState([]);
+
+	useEffect(() => {
+		let tempList = [];
+		for (let i = 0; i < articles.length; i++) {
+			let tagList = [];
+			let time = new Date(articles[i].timestamp);
+			for (let j = 0; j < articles[i].tags.length; j++) {
+				tagList.push(
+					<div
+						key={
+							"tag-" +
+							articles[i].path +
+							"-" +
+							articles[i].tags[j]
+						}
+						className="badge badge-primary mr-1 p-3 group-hover:bg-base-100 border-none group-hover:text-base-content select-none"
+					>
+						#{articles[i].tags[j]}
+					</div>
+				);
+			}
+			tempList.push(
+				<Link href={"/" + id + "/" + articles[i].path}>
+					<div
+						key={"articles-" + i}
+						className={`group ${
+							i % 2 ? "bg-neutral-focus" : ""
+						} hover:bg-primary hover:text-primary-content duration-300 p-5 cursor-pointer`}
+					>
+						<div className="flex justify-between">
+							<div className="text-2xl">{articles[i].title}</div>
+							{time.toLocaleDateString("zh-TW")}
+						</div>
+						<div className="flex justify-start mt-2">{tagList}</div>
+					</div>
+				</Link>
+			);
+		}
+		setArticleCards(tempList);
+	}, [articles, id]);
+
 	return (
 		<>
+			<Head>
+				<title>{category.name + " | " + config.name}</title>
+			</Head>
 			<Layout>
 				<main>
-					Category
-					<br />
-					{articles}
-					{/* {console.log(category)} */}
+					<div className="container w-9/12 mx-auto my-4 rounded-xl pt-10 pb-4 select-none">
+						<h2 className="text-4xl text-primary">
+							{category.name}
+						</h2>
+						<p>{category.description}</p>
+					</div>
+					<div className="container w-9/12 mx-auto bg-neutral rounded-xl shadow-xl hover:shadow-2xl duration-300 transition-all overflow-hidden">
+						{articleCards}
+					</div>
 				</main>
 			</Layout>
 		</>
@@ -40,22 +92,42 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-	const articles = fs.readdirSync(
+	let articles = fs.readdirSync(
 		path.join(process.cwd(), "content", params.category)
 	);
 
-	// TODO Retreive category.json
 	const categoryDataRaw = fs.readFileSync(
 		path.join(process.cwd(), "content", params.category, "category.json")
 	);
 	const categoryData = JSON.parse(categoryDataRaw.toString());
 
-	// TODO Retreive a list of books' article.json in the category
+	let articleData = [];
+	articles = articles.filter((item) => {
+		return item !== "category.json";
+	});
+	for (let i = 0; i < articles.length; i++) {
+		let articleJson = fs.readFileSync(
+			path.join(
+				process.cwd(),
+				"content",
+				params.category,
+				articles[i],
+				"article.json"
+			)
+		);
+		let articleJsonParsed = JSON.parse(articleJson.toString());
+		articleJsonParsed["path"] = articles[i];
+		articleData.push(articleJsonParsed);
+	}
+
+	articleData.sort((a, b) => {
+		return -(Date.parse(a.timestamp) - Date.parse(b.timestamp));
+	});
 
 	return {
 		props: {
 			id: params.category,
-			articles,
+			articles: articleData,
 			category: categoryData,
 		},
 	};

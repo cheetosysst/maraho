b63bb0557cbd11fe3c89fd6544b325007184965e
cheetosysst <thect@protonmail.com>
2023-02-07 14:11:15 +0800

    File name change

--- components/main.layout.jsx
+++ components/main.layout.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import config from "../config";
-import Navbar from "./navbar.component.js";
+import Navbar from "./navbar.component.jsx";
 
 export default function Layout({ children }) {
 	const [links, setLinks] = useState([]);

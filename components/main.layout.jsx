63edb8f91dab621a6587149982f97bc3b7cd1898
cheetosysst <thect@protonmail.com>
2023-08-05 17:43:03 +0800

    fix: remove unuse fragment

--- components/main.layout.jsx
+++ components/main.layout.jsx
@@ -2,15 +2,13 @@ import Navbar from "./navbar.component.jsx";
 
 export default function Layout({ children }) {
 	return (
-		<>
-			<div className="flex flex-row">
-				<div className="flex-grow"></div>
-				<div className="w-[90%] md:w-[80%] xl:w-[65%]">
-					<Navbar />
-					<div className="px-4">{children}</div>
-				</div>
-				<div className="flex-grow"></div>
+		<div className="flex flex-row">
+			<div className="flex-grow"></div>
+			<div className="w-[90%] md:w-[80%] xl:w-[65%]">
+				<Navbar />
+				<div className="px-4">{children}</div>
 			</div>
-		</>
+			<div className="flex-grow"></div>
+		</div>
 	);
 }
